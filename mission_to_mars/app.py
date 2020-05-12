b19e766d6430cae9fdc4cd3c8a50ae011f72357a
Sguthrie123 <42792003+Sguthrie123@users.noreply.github.com>
2020-05-11 21:58:52 -0400

    Added comments to code

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -1,25 +1,28 @@
+# Import Dependencies
 from flask import Flask, render_template, redirect
 import pymongo
 import scrape
 
-
+# Establish Connection 
 conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
 db = client.MarsDB
 
+# Start the flask app
 app = Flask(__name__)
 
+# Define route 
 @app.route('/')
 def home():
-    
+    # find the db collection and render it 
     mars = db.db.collection.find()
     return render_template("index.html", mars = mars )
 
 @app.route('/scrape')
 def scraper():
-
+    # Run the scrape function we made to pull all the data from the sources 
     mars_data = scrape.scrape()
-
+    # Update the collection with new data 
     db.db.collection.update({} ,mars_data ,upsert=True )
 
     return redirect("/")
